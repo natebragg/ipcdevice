c7b51faa3684b3169373355f92f2200d5da096fc
Nate Bragg <jonathan.bragg@alum.rpi.edu>
2012-03-21 20:03:58 -0400

    Reworked writing to be circular buffer based.

--- ipcdevice.c
+++ ipcdevice.c
@@ -37,16 +37,26 @@
 #include <linux/fs.h>
 #include <linux/slab.h>
 #include <linux/sched.h>
+#include <linux/wait.h>
 #include <asm/uaccess.h>
 
-static char *blackboard;
-static const int bb_size = 4096;
+static char *blackboard, *bb_rhead, *bb_whead;
+static const int BB_SIZE = 4096;
 DECLARE_WAIT_QUEUE_HEAD (bbq_read);
+DECLARE_WAIT_QUEUE_HEAD (bbq_write);
 
 int ipcdevice_open(struct inode*, struct file*);
 int ipcdevice_release(struct inode*, struct file*);
 static ssize_t ipcdevice_read(struct file*, char __user*, size_t, loff_t*);
 static ssize_t ipcdevice_write(struct file*, const char __user*, size_t, loff_t*);
+
+inline unsigned int _min(unsigned int a, unsigned int b){
+    return (a<b)?a:b;
+}
+
+inline unsigned int _max(unsigned int a, unsigned int b){
+    return (a>b)?a:b;
+}
 
 const struct file_operations ipcdevice_fops = {
     .open  = ipcdevice_open,
@@ -55,21 +65,20 @@ const struct file_operations ipcdevice_fops = {
     .write = ipcdevice_write,
 };
 
-int ipcdevice_open(struct inode *inode, struct file *filp)
+int ipcdevice_open(struct inode *inode, struct file *file)
 {
     return 0;
 }
 
-int ipcdevice_release(struct inode *inode, struct file *filp)
+int ipcdevice_release(struct inode *inode, struct file *file)
 {
     return 0;
 }
 
 static ssize_t ipcdevice_read(struct file *file, char __user *buf,
         size_t count, loff_t *ppos){
-    const size_t len = strnlen(blackboard, bb_size) + 1;
+    const size_t len = strnlen(blackboard, BB_SIZE) + 1;
     int result;
-    int to_end;
     size_t to_read;
 
     if( blackboard[0] == 0 ){
@@ -81,9 +90,7 @@ static ssize_t ipcdevice_read(struct file *file, char __user *buf,
             return result;
     }
 
-    to_end = len-(int)*ppos;
-    to_end = to_end > 0 ? to_end : 0;
-    to_read = (to_end >= count) ? count : to_end;
+    to_read = _min(_max(len-(int)*ppos, 0), count);
     if ( to_read == 0 )
         return 0;
     copy_to_user(buf, blackboard+*ppos, to_read);
@@ -94,19 +101,40 @@ static ssize_t ipcdevice_read(struct file *file, char __user *buf,
 static ssize_t ipcdevice_write(struct file *file, const char __user *buf,
         size_t count, loff_t *ppos){
     size_t null_term_offset = buf[count-1] == 0 ? 0 : 1;
-    if( count+null_term_offset > bb_size ){
-        return 0;
+    size_t to_write = 0, head_space = 0, written = 0;
+    int result = 0;
+
+    while( count > 0 ){
+        result = wait_event_interruptible(bbq_write, (bb_rhead != bb_whead+1) );
+        if( result != 0 )
+            return result;
+
+        //we can write all the way up to bb_rhead, circularly
+        head_space = (BB_SIZE + (bb_rhead - bb_whead) - 1) % BB_SIZE;
+        to_write = _min(head_space, _min(BB_SIZE-(bb_whead-blackboard), count));
+        copy_from_user(bb_whead, buf+written, to_write);
+        bb_whead = blackboard + ((bb_whead - blackboard + to_write) % BB_SIZE);
+        written += to_write;
+        count -= to_write;
     }
-    copy_from_user(blackboard,buf,count);
-    blackboard[count-1+null_term_offset] = 0;
+    if( null_term_offset > 0){
+        result = wait_event_interruptible(bbq_write, (bb_rhead != bb_whead+1) );
+        if( result != 0 )
+            return result;
+
+        //Counting this towards written would throw off the writer, so don't.
+        *bb_whead = 0;
+        bb_whead = blackboard + ((bb_whead - blackboard + null_term_offset) % BB_SIZE);
+    }
     wake_up_interruptible_sync(&bbq_read);
 
-    return count;
+    *ppos = (bb_whead-blackboard);
+    return written;
 }
 
 int __init ipcdevice_init(void){
     int result;
-    printk( KERN_INFO "Installing ipcdevice module\n");
+    printk( KERN_INFO "ipcdevice: installing module\n");
     result = register_chrdev(IPC_MAJOR, IPC_NAME, &ipcdevice_fops);
     if( result < 0 ){
         printk( KERN_ERR "ipcdevice: error registering major number %d\n",
@@ -114,7 +142,7 @@ int __init ipcdevice_init(void){
         return result;
     }
 
-    blackboard = kmalloc(bb_size, GFP_KERNEL);
+    bb_rhead = bb_whead = blackboard = kmalloc(BB_SIZE, GFP_KERNEL);
     if (blackboard == NULL){
         unregister_chrdev(IPC_MAJOR, IPC_NAME);
         return -ENOMEM;
